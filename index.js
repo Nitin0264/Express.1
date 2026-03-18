11e5c56970a54b5b6f65d48d3dcfdb4766333830
Nitin0264 <nc8224140@gmail.com>
2026-03-18 22:21:39 +0530

    day -2

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ let app = express();
 app.get("/",(req,res) => {
   res.send("hello there the name chauhan Nitin chauhan")
 });
-app.get("/home",(req,res)=>
-{
-  res.send("using this for the products")
-})
+
 app.get("/var",(req,res)=> {
   let vari  = "hi i am nitin how can i assist you"
   res.send(vari);
